c361d4e0f9376ffb0d4b00a4b4f7dee27835df38
ali-mmagneto <92598622+ali-mmagneto@users.noreply.github.com>
2022-08-15 22:22:08 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 from os import environ
 
-BOT_TOKEN = environ.get("BOT_TOKEN", none)
-API_ID = environ.get("API_HASH", none)
-API_HASH = environ.get("API_HASH", none)
+BOT_TOKEN = environ.get("BOT_TOKEN", None)
+API_ID = environ.get("API_HASH", None)
+API_HASH = environ.get("API_HASH", None)
 DOWNLOAD_DIR = "./downloads"
