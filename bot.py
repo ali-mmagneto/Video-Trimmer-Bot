be21fe103d27bb552d015cc529302db3a1e1bd9b
ali-mmagneto <92598622+ali-mmagneto@users.noreply.github.com>
2022-08-15 21:09:32 +0300

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,46 @@
+from pyrogram import Client
+from trimmer import trim
+
+video_mimetype = [
+    "video/x-flv",
+    "video/mp4",
+    "video/avi",
+    "video/mkv",
+    "application/x-mpegURL",
+    "video/mp2t",
+    "video/3gpp",
+    "video/quicktime",
+    "video/x-msvideo",
+    "video/x-ms-wmv",
+    "video/x-matroska",
+    "video/webm",
+    "video/x-m4v",
+    "video/quicktime",
+    "video/mpeg"
+]
+
+
+
+@Client.on_message((filters.video | filters.document)
+async def video(bot, message):
+    if message.document:
+        if not message.document.mime_type in video_mimetype:
+            message.reply_text("```Geçersiz Video !\nBu video dosyasına benzemiyor.```", quote=True)
+            return
+    try:
+        ss = await bot.send_message("Başlangıç süresini yaz.\n\n örnek: `01:20:69`", reply_markup=ForceReply(True))
+        s = await ss.get_reply()
+        baslangic = s.text
+        await ss.delete()
+    except Exception as e:
+        print(e)
+        return await bot.send_message("hata oldu")
+    try:
+        dd = await bot.send_message("bitiş süresini yaz.\n\n örnek: `01:50:69`", reply_markup=ForceReply(True))
+        d = await d.get_reply()
+        bitis = d.text
+        await dd.delete()
+    except Exception as f:
+        print(f)
+        return await bot.send_message("hata oldu")
+    await trim(message, baslangic, bitis)
